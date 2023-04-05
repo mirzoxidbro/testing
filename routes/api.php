d1ec2fdd60e8a004be61700597ab6ab20a30cc9a
mirzoxid <mirzoxidadxamjanov01@gmail.com>
2023-04-05 17:48:20 +0500

    excel export

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ExportExcelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::controller(ExportExcelController::class)->group(function(){
+    Route::get('index', 'index');    
+    Route::get('export/excel', 'export')->name('export.excel');
 });
