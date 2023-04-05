6e05928a48543b85864624ce53cdf5ce061384c3
mirzoxid <mirzoxidadxamjanov01@gmail.com>
2023-04-05 11:49:55 +0500

    payment integration

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::any('/handle/{paysys}',function($paysys){
+    (new Goodoneuz\PayUz\PayUz)->driver($paysys)->handle();
+});
+
+
+Route::any('/pay/{paysys}/{key}/{amount}',function($paysys, $key, $amount){
+	$model = Goodoneuz\PayUz\Services\PaymentService::convertKeyToModel($key);
+    $url = request('redirect_url','/'); // redirect url after payment completed
+    $pay_uz = new Goodoneuz\PayUz\PayUz;
+    $pay_uz
+    	->driver($paysys)
+    	->redirect($model, $amount, 860, $url);
+});
